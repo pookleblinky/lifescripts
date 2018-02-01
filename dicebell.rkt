1b60fc913100ba2d2701a9f7a66056b90e3e540f
pookleblinky <pookleblinky@gmail.com>
2018-02-01 01:49:03 -0500

    Simplified dicebell

--- dicebell.rkt
+++ dicebell.rkt
@@ -1,13 +1,16 @@
 #lang racket
 
-;; I've been determining kettlebell swings by rolling 2d6 and multiplying by 10.
+;; I've been determining kettlebell swings by rolling 2d6 and multiplying by
+;10. But, I may as well simplify a bit. Instead, n*d6. 40 gives a minimum of 40
+;swings, an average of 140, and a max of 240. Go up to 50 and the max hits
+;300.
 
 (require "rng.rkt")
 
 (provide doswings)
 
 ;; TODO: Make multipler toggle based on day's coinflip
-(define multiplier 10)
+(define multiplier 40)
 
 (define swings (* multiplier (diceroll)))
 
