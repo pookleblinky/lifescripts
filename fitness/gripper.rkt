b3e03c944d51eb1e9e1a81ebdd2dbf75c48bd846
pookleblinky <pookleblinky@gmail.com>
2018-02-01 22:40:20 -0500

    gripper script

---
+++ fitness/gripper.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require "../machinery/rng.rkt" "../machinery/savingthrow.rkt")
+
+;; Idea: Roll to see whether to do a set of grippers, and which one to do.
+
+(define laziness 7)
+
+(define (gripper?)
+  (define result (savingthrow laziness))
+  (if (equal? #t result) #t #f))
+
+(define (reps)
+  (roll2d6))
+
+(define (crush)
+  (if (equal? #t (gripper?)) 
+  (printf "Do ~a reps" (reps))
+  (printf "Relax")))
+
+(crush)
