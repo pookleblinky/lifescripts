e93d74c74879f81894901cb324df0c87d31bf9d4
pookleblinky <pookleblinky@gmail.com>
2018-02-03 16:45:54 -0500

    Bored: drink water or crush gripper?

--- bored.rkt
+++ bored.rkt
@@ -1,3 +1,13 @@
 #lang racket
 
-(require "fitness/hydrate.rkt" "fitness/gripper.rkt")
+(require "machinery/rng.rkt"
+         "fitness/hydrate.rkt"
+         "fitness/gripper.rkt")
+
+;; Right now: sip some water or squeeze a gripper?
+
+(define (bored)
+  (define result (coinflip))
+  (if (equal? result "HEADS")
+    (drink?)
+    (crush)))
