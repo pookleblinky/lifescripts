b6ff4831428905f6e4679807ac7b8d445d83d6fb
pookleblinky <pookleblinky@gmail.com>
2018-02-03 18:07:36 -0500

    bored now has a bunch of little activities

--- bored.rkt
+++ bored.rkt
@@ -4,13 +4,21 @@
          "fitness/hydrate.rkt"
          "fitness/gripper.rkt")
 
-;; Right now: sip some water or squeeze a gripper?
+(provide bored)
+
+;; TODO: Have hydrate/gripper/etc pipe into activities
+
+(define activities
+  (list
+    "Do 20 kettlebell swings"
+    "Do 10 pullups"
+    "Do 20 band pull aparts"
+    "Make a cup of coffee"
+    "Check twitter"))
 
 (define (bored)
-  (define result (coinflip))
-  (if (equal? result "HEADS")
-    (drink?)
-    (crush)))
+  (define result (random (length activities)))
+  (printf "~a~n" (list-ref activities result)))
 
 (bored)
 ;; TODO: Make it easy to extend to such as a round of swings, pullups, etc
