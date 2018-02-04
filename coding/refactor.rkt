3d3b5197dd0ec0009b7b140da635e41104af4722
pookleblinky <pookleblinky@gmail.com>
2018-02-03 23:50:13 -0500

    Cleaned up refactor?

--- coding/refactor.rkt
+++ coding/refactor.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
 ;; I have a problem when coding. I have a hard time deciding whether to do new
-;stuff, or clean up and refactor what I've already done. Sometimes my
-;productivity just crashes because I'm torn between trying to do both at once,
-;and I end up getting absolutely nothing done.
+;; stuff, or clean up and refactor what I've already done. Sometimes my
+;; productivity just crashes because I'm torn between trying to do both at once,
+;; and I end up getting absolutely nothing done.
 
 (require "../machinery/rng.rkt" "../machinery/savingthrow.rkt")
 
@@ -13,10 +13,11 @@
 
 (define (refactor?)
   (define result (savingthrow neatness))
-  (if (equal? #t result) #t #f))
+  (equal? #t result))
 
 (define (dotask)
   (if (equal? #t (refactor?))
     (printf "Clean up some code.~n")
     (printf "Write some new code.~n")))
 
+(dotask)
