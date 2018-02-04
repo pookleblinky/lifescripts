ca162f4369a141d7ac10a1771d4a20c667ab85d6
pookleblinky <pookleblinky@gmail.com>
2018-02-04 04:07:00 -0500

    attention boost topic

--- social/attention.rkt
+++ social/attention.rkt
@@ -2,3 +2,19 @@
 
 ;;; tiny proof of concept. What I want now is to simply choose, at random, one
 ;issue/news event to focus on at a time. 
+
+(require "../machinery/rng.rkt")
+
+(provide boost-topic)
+
+(define topics
+  (list
+    'foo
+    'bar
+    'baz))
+
+(define (boost-topic)
+  (define topic (randomchoice topics))
+  (printf "Today's topic to boost: ~a~n" topic))
+
+(boost-topic)
