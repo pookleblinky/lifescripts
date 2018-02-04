4ec93ed3b5d1aaa7557a368305e263c4c5fea62d
pookleblinky <pookleblinky@gmail.com>
2018-02-04 03:41:09 -0500

    More genres

--- music.rkt
+++ music.rkt
@@ -4,26 +4,41 @@
 
 (provide listen)
 
+;; These are all valid genres.
 (define genres
   (list
+    'MFDOOM
+    'OST
     'bebop
+    'biggie
     'bluegrass
+    'boardsofcanada
+    'cake
     'chicagoblues
     'classical
     'deltablues
     'dubstep
+    'femalejazzvocals
+    'glitchcore
+    'gorillaz
     'griot
     'hendrix
+    'hiphop
     'indian
     'jazz
     'klezmer
     'metal
+    'nightcore
     'nightvale
+    'numetal
     'paganini
     'poetry
     'sufi
+    'theloniousmonk
     'tomwaits
-    'vocals))
+    'vocals
+    'witchhouse
+    'wutang))
 
 (define (listen)
   (define genre (random (length genres)))
