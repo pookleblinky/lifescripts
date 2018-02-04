1d8b61a9c58aa353fd701b389e24c99d7df3eaa8
pookleblinky <pookleblinky@gmail.com>
2018-02-03 23:57:48 -0500

    cleaned up conditional

--- fitness/hydrate.rkt
+++ fitness/hydrate.rkt
@@ -10,7 +10,9 @@
 
 (define (drink?)
   (define result (savingthrow thirst))
-  (if (equal? #f result)
+  (if (not result)
     (printf "Guzzle a cupful.~n")
     (printf "You're good.~n")))
+
+(drink?)
 
