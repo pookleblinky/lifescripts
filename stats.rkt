3e28691d857fae13f74425556d1b9f4808de452c
pookleblinky <pookleblinky@gmail.com>
2018-02-05 11:41:49 -0500

    reverse engineer the fucks

--- stats.rkt
+++ stats.rkt
@@ -2,23 +2,25 @@
 
 ;; Existing stats
 (define fucks 4)
-(define cusses 12)
+(define profanity 12)
 
 ;; Cardinal stats (CoC)
 
 ; Primary
-(define strength 0)
-(define constitution 0)
-(define dexterity 0)
-(define size 0)
-(define intelligence 0)
-(define power 0)
-(define appearance 0)
-(define education 0)
+(define strength 13)
+(define constitution 18)
+(define dexterity 6)
+(define size 12)
+(define intelligence 8)
+(define power 10)
+(define appearance 10)
+(define education 6)
 
 ; Secondary
 (define idea (* 5 intelligence))
-(define knowledge (*5 education))
+(define knowledge (* 5 education))
 (define luck (* 5 power))
 (define hitpoints (ceiling (/ (+ size constitution) 2)))
 (define sanity (* 5 power))
+
+;; Reverse engineering fucks and profanity
