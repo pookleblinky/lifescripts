dec5bb9f980328efb4f4a1b9513c4dd884db6c90
pookleblinky <pookleblinky@gmail.com>
2018-02-05 16:57:10 -0500

    skills

--- skills.rkt
+++ skills.rkt
@@ -2,3 +2,9 @@
 
 ;; I may as well go all in after making stats.
 ;; skill = x. Roll < x to succeed. Roll > x to increase
+;; http://the-call-of-cthulhu.obsidianportal.com/wikis/occupation-and-skills
+
+;; Pick 8 skills. Points = (* 20 education) to distribute
+
+(require "stats.rkt")
+
