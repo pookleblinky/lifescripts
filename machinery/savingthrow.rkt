0ee026bfebd4e5c3ab84463f0703d738776e70ad
pookleblinky <pookleblinky@gmail.com>
2018-02-05 14:08:35 -0500

    savingthrow hmmm

--- machinery/savingthrow.rkt
+++ machinery/savingthrow.rkt
@@ -6,8 +6,12 @@
 ; My original model of saving throws was wrong.
 ; I'm using Call of Cthulhu stats, too, so it was not even wrong.
 
+;; Given an attribute or skill, roll a d100. If the result is lower, success.
+;; But, I started off backwards and upside down. 
+
 (provide savingthrow)
 
 (define (savingthrow numbertobeat)
   (define result (rolld20))
   (> result numbertobeat))
+
