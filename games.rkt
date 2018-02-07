c00a8f580fa766caebd1c066b2a1ced23f2506ab
pookleblinky <pookleblinky@gmail.com>
2018-02-06 20:55:50 -0500

    a script to decide what game to play

---
+++ games.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;; I hate games, and can usually only stand to play about 20 minutes total per
+;year.
+
+(require "machinery/rng.rkt")
+
+(provide playthis)
+
+(define games
+  (list
+    'nethack
+    'angband
+    'moria
+    'brogue))
+
+(define (playthis)
+  (define game (randomchoice games))
+  (printf "Play ~a~n" game))
+
+(playthis)
