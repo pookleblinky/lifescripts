a25746ba4283997d8bad5c5baadea3e4790885f4
pookleblinky <pookleblinky@gmail.com>
2018-05-01 13:24:04 -0400

    dicefasting with 2d6 migration

---
+++ fitness/dicefast.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+;; Going to switch from coin to 2d6. Rolling a 6- will happen ~41% of the time,
+;; or about 3/7 days. A bit under the EV of a coin. 7-9 will happen about 42% of
+;; the time. 10+ about 16.6% of the time.
+
+;; This'd give more range of gradations, and room for using a stat to adjust the
+;; frequency in a single spot (say, start with -1 and over time increase it to
+;; +1). 
